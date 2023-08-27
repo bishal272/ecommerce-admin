5bb0587fabc48e17609e9897db15fe46d109bda6
bishal272 <merkasin272@gmail.com>
2023-08-27 16:50:08 +0530

    aws s3 upload images

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,9 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
 import multiparty from "multiparty";
+const bucketName = "ecommerce-merkasin-app";
+
 export default async function handler(req, res) {
   const form = new multiparty.Form();
   const { fields, files } = await new Promise((resolve, reject) => {
@@ -8,9 +13,31 @@ export default async function handler(req, res) {
       resolve({ fields, files });
     });
   });
-  console.log("length:", files.file.length);
-  console.log(fields);
-  res.json("ok");
+  const client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+  const links = [];
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFileName = Date.now() + "." + ext;
+    client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFileName,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
+    links.push(link);
+  }
+
+  res.json({ links });
 }
 
 //stops nextjs from parsing the files to json
