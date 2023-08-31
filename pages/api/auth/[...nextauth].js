feaa15069090d45bd45c4f96a36d882806bdf7e8
bishal272 <merkasin272@gmail.com>
2023-08-31 20:19:34 +0530

    admin security added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
 import clientPromise from "@/lib/mongodb";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+const adminEmails = ["merkasin272@gmail.com"];
+
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
 
@@ -13,4 +15,23 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-});
+  callbacks: {
+    session: ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw "Not an admin";
+  }
+}
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,9 +2,13 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import fs from "fs";
 import mime from "mime-types";
 import multiparty from "multiparty";
+import { isAdminRequest } from "./auth/[...nextauth]";
+import { mongooseConnect } from "@/lib/mongoose";
 const bucketName = "ecommerce-merkasin";
 
 export default async function handler(req, res) {
+  await mongooseConnect();
+  await isAdminRequest(req, res);
   const form = new multiparty.Form();
   const { fields, files } = await new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
