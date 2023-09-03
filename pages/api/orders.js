36d178d9d09c82928937cc39b9737bbf8dc5199d
bishal272 <merkasin272@gmail.com>
2023-09-04 02:03:09 +0530

    added order delete function

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -3,5 +3,12 @@ import { Order } from "@/models/Order";
 
 export default async function handler(req, res) {
   await mongooseConnect();
-  res.json(await Order.find().sort({ createdAt: -1 }));
+  if (req.method == "GET") {
+    res.json(await Order.find().sort({ createdAt: -1 }));
+  }
+  if (req.method == "DELETE") {
+    const { _id } = req.query;
+    await Order.deleteOne({ _id });
+    res.json("ok");
+  }
 }
--- pages/settings.js
+++
@@ -1,9 +0,0 @@
-import Layout from "@/components/layout";
-
-export default function Settings() {
-  return (
-    <Layout>
-      <h1>settings</h1>
-    </Layout>
-  );
-}
